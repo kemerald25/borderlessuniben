253dd044acaed255941af6f6880f5fd303b4d18a
Kemerald25 <unityekeoba18@gmail.com>
2025-08-02 03:54:25 +0100

    updates

--- Day1/config/wallet.ts
+++ Day1/config/wallet.ts
@@ -1,17 +1,14 @@
-
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
-
 import { mainnet, sepolia } from 'viem/chains'
 
 // 1. Get projectId at https://cloud.walletconnect.com
-// This is a public sample ID. It's highly recommended to get your own.
-const projectId = 'c02e88a0f252749cb0d9888944510b64'
+const projectId = import.meta.env.VITE_PROJECT_ID
 
-// 2. Create wagmiConfig
+// Rest of your code remains the same...
 const metadata = {
   name: 'Stellar DApp',
   description: 'A decentralized application with WalletConnect',
-  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  url: 'https://web3modal.com',
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
@@ -20,10 +17,8 @@ export const config = defaultWagmiConfig({
   chains,
   projectId,
   metadata,
-  // ...wagmiOptions // Optional - Override createConfig parameters
 })
 
-// 3. Create modal
 createWeb3Modal({
   wagmiConfig: config,
   projectId,
